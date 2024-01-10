3520eb906197343ea13feb8f86fbcf470b85777b
MazenEllithy98 <mazen.a.ellethy@gmail.com>
2024-01-10 04:36:10 +0200

    Add project files.

---
+++ 3d_Point.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAssignment3
+{
+    internal class _3d_Point : IComparable , ICloneable 
+    {
+        public int x { get; set; }
+
+        public int y { get; set; }
+
+
+        public int z { get; set; }
+
+
+        public _3d_Point(int _x, int _y)
+        {
+
+        }
+
+        // constructor chaining
+        public _3d_Point(int _x, int _y, int _z)
+        {
+            this.x = _x;
+            this.y = _y;
+            this.z = _z;
+        }
+
+        public int CompareTo(_3d_Point other)
+        {
+            if (this.x == other.x)
+            {
+                return this.y.CompareTo(other.y);
+            }
+            else
+            {
+                return this.x.CompareTo(other.x);
+            }
+
+        }
+        public _3d_Point(_3d_Point pointcopy)
+        {
+            this.x = pointcopy.x;
+            this.y = pointcopy.y;
+            this.z = pointcopy.z;
+        }
+
+
+        public object Clone()
+        {
+            return new _3d_Point(x,y , z)
+            { 
+                x = this.x,
+                y = this.y,
+                z = this.z
+            };
+        }
+
+        //string override method
+        public override string ToString()
+        {
+            return $"Point Coordinates ({x},{y},{z})";
+        }
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿namespace OOPAssignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //_3d_Point P1 = new _3d_Point(1, 1, 1);
+            //Console.WriteLine("Please Enter the value for the first Point P1 (x,y,z) ");
+            //P1.x = int.Parse(Console.ReadLine());
+            //if (int.TryParse(Console.ReadLine(), out int y1))
+            //{
+            //    P1.y = y1;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The input for coordinate y is not an integer(Default Value is Used");
+            //}
+            //P1.z = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(P1.ToString());
+
+            //_3d_Point P2 = new _3d_Point(2, 2, 2);
+            //Console.WriteLine("Please Enter the value for the second Point P1 (x,y,z) ");
+            //P2.x = int.Parse(Console.ReadLine());
+            //if (int.TryParse(Console.ReadLine(), out int y2))
+            //{
+            //    P2.y = y2;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The input for coordinate y is not an integer(Default Value is Used");
+            //}
+            //P2.z = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(P2.ToString());
+
+            //if (P1 == P2)
+            //{
+            //    Console.WriteLine("The two points are equal");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The two points are not equal");
+            //}
+
+            // the == operator does not work properly
+
+
+            //_3d_Point[] Points = new _3d_Point[3];
+            
+            // Points[0] =new _3d_Point( 6, 3 , 2);
+            // Points[1] =new _3d_Point( 2, 2 , 2);
+            // Points[2] =new _3d_Point( 2, 1 , 2);
+
+
+            //Array.Sort(Points);
+
+
+            //for (int i = 0; i < Points.Length; i++)
+            //{
+            //    Console.WriteLine(Points[i]);
+            //}
+
+            //Points[0] = (_3d_Point)Points[1].Clone();
+            //Console.WriteLine(Points[0]);
+            //Console.WriteLine(Points[1]);
+
+        }
+    }
+}
